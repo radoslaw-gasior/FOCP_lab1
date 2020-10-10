42f5039d53aad705f3c91bc21e27cef4669b0978
Radosław Gąsior <radogas429@student.polsl.pl>
2020-10-10 11:54:19 +0200

    next changes

--- project1/project1/project1.cpp
+++ project1/project1/project1.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 int main(){
- 
+ //PART1
+	/*
 	cout << "Hello world" << endl;
 
 	//return 0;
@@ -12,8 +13,20 @@ int main(){
 	cout << a << "	" << b << endl;
 
 	return 0;
+	*/
 
+//PART2  
 
+	int number;
 
+	cout << "Introduce number:" << endl;
+	cin >> number;
+
+	if (number % 2 == 0) {
+		cout << "Number is even" << endl;
+	}
+	else {
+		cout << "Number is odd" << endl;
+	}
 }
 

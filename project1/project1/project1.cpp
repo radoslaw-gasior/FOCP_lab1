c1cc1696a9b7d342cd0631cf0f6ff33a65934620
Radosław Gąsior <radogas429@student.polsl.pl>
2020-10-10 12:05:08 +0200

    equation solver

--- project1/project1/project1.cpp
+++ project1/project1/project1.cpp
@@ -16,7 +16,7 @@ int main(){
 	*/
 
 //PART2  
-
+	/*
 	int number;
 
 	cout << "Introduce number:" << endl;
@@ -28,5 +28,22 @@ int main(){
 	else {
 		cout << "Number is odd" << endl;
 	}
+	*/
+
+//PART3
+
+	// ax+b=c
+	//ax=c-b
+	//x=(c-b)/a
+
+	float a, b, c;
+	cout << "Please input a, b and c accoriding to:" << endl << "Ax+B=C" << endl;
+	cin >> a >> b >> c;
+
+	float x;
+
+	x = (c - b) / a;
+
+		cout << "X equals to" << x << endl;
 }
 
